dcd566b084da7543665eb1c64780e2eb33aeec94
“im-minji” <mjlim514@gmail.com>
2025-10-18 15:21:06 +0900

    feat: 사용자 입력 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,33 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+//      1. 입력
+//      덧셈할 문자열을 입력해 주세요. 출력
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+//      문자열 입력 (Console.readLine() 사용)
+        String input = Console.readLine();
+
+//      2. 기본 구분자로 String 배열 생성
+//      , 과 : 을 구분자로 활용하여 숫자 추출
+
+//      3. 커스텀 구분자로 String 배열 생성
+//      커스텀 문자를 추출하여 숫자 추출 (//<커스텀 구분자>\n)
+
+//      4. 덧셈 로직
+//      String 배열 요소 -> int 배열로 변경
+//      각 숫자들 덧셈
+
+//        5. 출력
+//        결과 : <결과값> 출력
+
+//        6. 예외처리
+//        사용자가 잘못된 값을 입력할 경우 ‘IllegalArgumentException’을 발생시킨 후 애플리케이션은 종료
+//        음수 입력, 형식(커스텀 문자 정의없이 커스텀 문자 사용)이 잘못된 문자열 예외 처리
+//        “” 빈 문자열 입력 시 0 반환
+//        종료 시 System.exit() 사용 금지
     }
 }
